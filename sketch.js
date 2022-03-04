1e141a1d6cb40d21a61b9068d3615ea204b270fb
JorgeSotoVertiz <90294706+JorgeSotoVertiz@users.noreply.github.com>
2022-03-03 21:00:52 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var uno
+
+function preload (){
+uno=loadImage("811d7f271312.gif");
+}
+
+
+function setup(){
+createCanvas(width,height)
+}
+
+function draw(){
+background("black");
+image(uno,0,0,width,height)
+}
